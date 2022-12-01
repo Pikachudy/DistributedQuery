967592de31c6ee8ff282213a5268c01282ca62df
JiaoJiao <923252409@qq.com>
2022-12-01 15:38:31 +0800

    修改所筛选数据标签，使xml划分相对更均匀

--- src/main/java/XmlPartition.groovy
+++ src/main/java/XmlPartition.groovy
@@ -1,7 +1,7 @@
 import javax.xml.stream.*
-pieces = 24 // 切分块数
+pieces = 6 // 切分块数
 input = "D:\\homework\\Distribution\\dblp\\dblp.xml" // xml源文件路径
-output = "output_%04d.xml" // 输出命名
+output = ".\\output_%04d.xml" // 输出命名
 eventFactory = XMLEventFactory.newInstance()
 fileNumber = elementCount = 0
 System.setProperty("entityExpansionLimit","3000000")
@@ -47,12 +47,15 @@ def createNextEventWriter () {
     return writer
 }
 elements = createEventReader().findAll { it.startElement && it.name == firstChild.name }.size()
+elements = 9600000
 println "Splitting ${elements} <${firstChild.name.localPart}> elements into ${pieces} pieces"
 chunkSize = elements/pieces
 writer = createNextEventWriter()
 writer.add(firstChild)
 createEventReader().each {
-    if (it.startElement && it.name == firstChild.name) {
+    if (it.startElement && (it.name == firstChild.name||((String)it.name).equals("inproceedings")||((String)it.name).equals("proceedings")||((String)it.name).equals("book")||((String)it.name).equals("incollection")||((String)it.name).equals("phdthesis")||((String)it.name).equals("mastersthesis")||((String)it.name).equals("www"))) {
+//        println it.name
+//        println chunkSize + " : " +elementCount
         if (++elementCount > chunkSize) {
             writer.add(eventFactory.createEndDocument())
             writer.flush()
